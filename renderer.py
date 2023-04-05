db0e9e2d6920f5f8f6eeb565986339ad8f289c8f
SedenionProj <96470946+SedenionProj@users.noreply.github.com>
2023-04-05 19:36:39 +0200

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1,171 +1,31 @@
-import renderer as renderer
-import time
-import keyboard
-from random import randint
+import os
 
-blockA = [[0,0,0,0],
-          [1,1,1,1],
-          [0,0,0,0],
-          [0,0,0,0]]
+longueur, hauteur = os.get_terminal_size()
 
-blockB = [[1,1],
-          [1,1]]
+image = [[' ' for _ in range(longueur)] for _ in range(hauteur)]
+hauteur -= 2
 
-blockC = [[0,1,0],
-          [1,1,1],
-          [0,0,0]]
+def placerPixel(x, y, char):
+    # place un pixel sur l'écran
+    x1 = round(x)
+    y1 = round(y)
+    if 0 <= x1 <= longueur - 1 and 0 <= y1 <= hauteur - 1:
+        image[y1][x1] = char
 
-blockD = [[0,1,0],
-          [0,1,1],
-          [0,0,1]]
 
-blockE = [[0,1,0],
-          [1,1,0],
-          [1,0,0]]
-
-blockF = [[0,1,1],
-          [0,1,0],
-          [0,1,0]]
-
-blockG = [[1,1,0],
-          [0,1,0],
-          [0,1,0]]
-
-tex = " -ABCDEFGHIJ" # texture des blocks
-
-def constructGrid(longueur):
-    # crée une grille de tetris vide
-    grille = [[0 for _ in range(longueur)] for _ in range(renderer.hauteur)]
-    for y in range(renderer.hauteur):
+def afficher(*info):
+    # affiche l'image à l'écran
+    info = "".join(info)
+    info += ' ' * (longueur - len(info))
+    strImage = ''
+    for y in range(hauteur):
         for x in range(longueur):
-            if x == 0 or x == longueur-1 or y == renderer.hauteur-1:
-                grille[y][x] = 1
-    return grille
+            strImage += image[y][x]
+    print(info + strImage, end='')
 
-def dessiner(grille,b,bText):
-    # dessine la grille dans la console
-    for y in range(len(grille)):
-        for x in range(len(grille[0])):
-            if grille[y][x] != 0:
-                renderer.placerPixel(x,y,tex[grille[y][x]])
-    # dessine le block dans la console
-    for y in range(len(b)):
-        for x in range(len(b[0])):
-            if b[y][x] == 1:
-                renderer.placerPixel(bx+x,by+y,tex[bTex])
-    
 
-def blockInit():
-    # réinitialise le block
-    randBlock = [blockA,blockB,blockC,blockD,blockE,blockF,blockG][randint(0,6)]
-    randTex = randint(2,len(tex)-1)
-    x = (len(grid[0]) - len(randBlock))//2
-    y = 0
-    return randBlock,randTex,x,y
-
-def collision(b,bx,by):
-    # detecte les collisions entre le block et la grille
-    for y in range(len(b)):
-        for x in range(len(b[0])):
-            if b[y][x] != 0 and grid[y+by][x+bx] != 0:
-                return True
-    return False
-
-def rotation(b,bx,by):
-    # applique une rotation au block
-    size = len(b)
-
-    rotated = [[0 for _ in range(size)] for _ in range(size)]
-
-    for x in range(size):
-        for y in range(size):
-            rotated[x][y] = b[size-y-1][x]
-
-    # detecte une collision après la rotation
-    if collision(rotated,bx,by):
-        return b
-    else:
-        return rotated
-
-def poserBlock(b,bx,by,bTex):
-    # dessine le block dans la grille
-    for y in range(len(b)):
-        for x in range(len(b[0])):
-            if b[y][x] == 1:
-                grid[y+by][x+bx] = bTex
-
-def detectLignes():
-    # enlève les lignes
-    check = True
-    for y in range(len(grid)-1):
-        check = True
-        for x in range(len(grid[0])):
-            if grid[y][x] == 0:
-                check = False
-        if check:
-            del grid[y]
-            grid.insert(0,[1]+[0]*(len(grid[0])-2)+[1])
-
-gridLongueur = 15
-
-grid = constructGrid(gridLongueur)
-
-dernier = time.time()
-temps = time.time()
-
-timer = 0
-speed = 0.5
-
-bType,bTex,bx,by = blockInit()
-
-hold = False
-
-while True:
-    renderer.supprimer()
-    actuelle = time.time()
-    dt = actuelle - dernier
-    dernier = actuelle
-
-    timer+=dt
-    
-    if keyboard.is_pressed("up"):
-        if not hold:
-            bType = rotation(bType,bx,by)
-        hold = True
-
-    elif keyboard.is_pressed("left"):
-        if not hold and not collision(bType,bx-1,by):
-            bx -= 1
-        hold = True
-
-    elif keyboard.is_pressed("right"):
-        if not hold and not collision(bType,bx+1,by):
-            bx += 1
-        hold = True
-
-    else:
-        hold = False
-
-    if keyboard.is_pressed("down"):
-        speed = 0.05
-    else:
-        speed = 0.5
-
-    if timer>=speed:
-        # executé tous les "speed" secondes
-        if collision(bType,bx,by+1):
-            if by == 0:
-                print("perdu !")
-                input()
-                exit()
-            else:
-                poserBlock(bType,bx,by,bTex)
-                detectLignes()
-                bType,bTex,bx,by = blockInit()
-        else:
-            by += 1
-        timer = 0
-
-    dessiner(grid,bType,bTex)
-
-    renderer.afficher()
+def supprimer():
+    # réinitialise l'image
+    for y in range(hauteur):
+        for x in range(longueur):
+            image[y][x] = ' '
